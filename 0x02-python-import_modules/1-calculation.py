5d63f32fc84dc877ec02838a4281f7fc65e956eb
Ckefa <ckefa65@gmail.com>
2023-03-09 07:00:24 -0800

    adding main stantement

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 
-from calculator_1 import add,sub,mul,div
+if __name__ == '__main__':
+    from calculator_1 import add,sub,mul,div
 
-print(add(10, 5))
-print(sub(10, 5))
-print(mul(10, 5))
-print(div(10, 5))
+    a = 10
+    b = 5
+
+    print(add(a, b))
+    print(sub(a, b))
+    print(mul(a, b))
+    print(div(a, b))
