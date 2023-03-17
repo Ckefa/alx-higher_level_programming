9dfa542c7dd1096dab12b4e2b90e097866761e25
Ckefa <ckefa65@gmail.com>
2023-03-17 09:25:16 -0700

    submit

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,4 +3,4 @@ def best_score(a_dictionary):
     if not a_dictionary:
         return "None"
     else:
-        return reduce(lambda x, y: y if y[1] > x else x, a_dictionary)
+        return reduce(lambda x, y: y[0] if y[1] > x else x, a_dictionary)
