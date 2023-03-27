f64621359d7452fd6c3979c085875334a4d26494
Ckefa <ckefa65@gmail.com>
2023-03-27 10:06:13 -0700

    submit

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
-def list_division(my_list_1, my_list_2, list_length):
 
+def safe_print_division(a, b):
+    """Returns the division of a by b."""
+    try:
+        div = a / b
+    except (TypeError, ZeroDivisionError):
+        div = None
+    finally:
+        print("Inside result: {}".format(div))
+    return (div)
