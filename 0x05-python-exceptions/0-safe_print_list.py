8ab30a6cb3acd5630cb0b228319c9845ba2e01b9
Ckefa <ckefa65@gmail.com>
2023-03-27 00:57:43 -0700

    submit

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    sum = 0
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
+            sum += 1
+    finally:
+        print("nb_print:", sum)
