0e377678a5604a92da308145a75c202fd80715eb
Ckefa <ckefa65@gmail.com>
2023-03-28 08:42:52 -0700

    submit

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -5,11 +5,7 @@
 
 class Square:
     def __init__(self, size=0):
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+        self.size = size
 
     @property
     """Getting the value ofsize."""
