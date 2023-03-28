4f9f9fc20f1406aae581da1490ccc7f16975a501
Ckefa <ckefa65@gmail.com>
2023-03-28 08:34:22 -0700

    submit

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""Define class square."""
+
+
+class Square:
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, val):
+        if not isinstance(val, int):
+            raise TypeErrorrr("size must be an integer")
+        elif val < 0:
+            raise TypeError("size must be >= 0")
+        self.__size = val
