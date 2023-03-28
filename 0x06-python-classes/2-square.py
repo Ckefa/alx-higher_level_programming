51297952cd5e1dd09d6d21212c1f07cdbd48bbb1
Ckefa <ckefa65@gmail.com>
2023-03-28 06:09:41 -0700

    submit

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,6 +7,10 @@ class Square:
     """Represent square."""
 
     def __init__(self, size=0):
+        """
+        Args:
+            size (int): The size of the square.
+        """
         self.__size = size
 
     @property
