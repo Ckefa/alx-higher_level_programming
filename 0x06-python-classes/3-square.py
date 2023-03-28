f41a327539b310c489c67106a0ccf63c0a0da271
Ckefa <ckefa65@gmail.com>
2023-03-28 07:28:50 -0700

    submit

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
 
-"""Define class square."""
+"""Define a class Square."""
 
 
 class Square:
-    """Represent a square"""
+    """Represent a square."""
 
     def __init__(self, size=0):
         """Initialize a new square.
-
         Args:
             size (int): The size of the new square.
         """
-        if isinstance(size, int):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
