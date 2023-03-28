92c422213fda2475d62d479d10581e7391d9d9d7
Ckefa <ckefa65@gmail.com>
2023-03-28 05:58:30 -0700

    submit

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""Defines class square."""
+
+
+class Square:
+    """Represent square."""
+
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, val):
+        if type(val) != int:
+            raise Exception("size must be an integer")
+        if val < 0:
+            raise ValueError
