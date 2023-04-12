6d053df225fd94c79c9c6f91a8fc1a754c34e98e
Ckefa <ckefa65@gmail.com>
2023-04-12 02:51:30 -0700

    submit

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,4 @@ def read_file(filename="", encoding="utf-8"):
     """This is the function for resding the file
     and printing the  ouptput to std out."""
     with open(filename, "r") as f:
-        print(f.read())
+        print(f.read(), end="")
