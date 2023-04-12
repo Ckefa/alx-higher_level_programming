89bed2c4ee96fca2ed3868100b4929812c377619
Ckefa <ckefa65@gmail.com>
2023-04-12 04:09:19 -0700

    submit

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
-"""This is the file handler module."""
+"""Defines a file-appending function."""
 
 
 def append_write(filename="", text=""):
-    """A a function that appends a string at the end of a text file."""
-    def append_write(filename="", text=""):
-        with open(filename, "a", encoding="utf-8") as f:
-            return f.write(text)
+    """Appends a string to the end of a UTF8 text file.
+    Args:
+        filename (str): The name of the file to append to.
+        text (str): The string to append to the file.
+    Returns:
+        The number of characters appended.
+    """
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
