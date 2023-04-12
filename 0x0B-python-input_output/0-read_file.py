e60908b07b156e2349a65527e2547df4b9922f49
Ckefa <ckefa65@gmail.com>
2023-04-12 02:50:07 -0700

    submit

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
-def read_file(filename=""):
+"""This is a module to manage files."""
+
+def read_file(filename="", encoding="utf-8"):
+    """This is the function for resding the file
+    and printing the  ouptput to std out."""
     with open(filename, "r") as f:
         print(f.read())
