8fb45d2f932f3a75b5a9cda275a4de2661ad1767
Ckefa <ckefa65@gmail.com>
2023-04-12 11:15:00 -0700

    submit

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -6,4 +6,4 @@ import json
 def load_from_json_file(filename):
     """Function to load json object from file."""
     with open(filename, "r", encoding="utf-8") as f:
-        return json.load(f.read())
+        return json.load(f)
