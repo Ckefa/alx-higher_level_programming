c4bde06b70b10b03aac983fd9cd0ef21fe06073b
Ckefa <ckefa65@gmail.com>
2023-04-12 05:47:38 -0700

    submit

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import json
+"""This is the main module."""
+
+
+def to_json_string(my_obj):
+    """This function converts an object into a json string."""
+    return json.dumps(my_obj)
