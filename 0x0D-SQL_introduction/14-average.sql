53afb4c83dada7a76f882c5a5c641ba0b2414d30
Ckefa <ckefa65@gmail.com>
2023-05-17 10:18:20 -0700

    submit

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,3 +1,3 @@
-SELECT AVG(score)
-FROM second_table
-AS average;
+-- Computes the average score of all records in the table second_table.
+SELECT AVG(`score`) AS `average`
+FROM `second_table`;
--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,1 +1,4 @@
-SELECT COUNT(id) FROM first_table WHERE id=89;
+-- Displays the number of records with id = 89 in the table first_table.
+SELECT COUNT(*)
+FROM `first_table`
+WHERE `id` = 89;
